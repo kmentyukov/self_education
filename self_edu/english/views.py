d5e7d88a7a8b42cd8b43d8526f20d3635a7a76d3
Kirill Mentyukov <rogue5@mail.ru>
2023-02-09 23:59:26 +0600

    add bootstrap

--- self_edu/english/views.py
+++ self_edu/english/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from english.models import NewWord
+
 
 def index_page(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'words': NewWord.objects.all()})
