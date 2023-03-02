23342029c8abe7aaa66a29e87faeba4f6238da84
Kirill Mentyukov <rogue5@mail.ru>
2023-03-02 17:58:36 +0600

    add debug_django_tools

--- self_edu/self_edu/urls.py
+++ self_edu/self_edu/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
@@ -29,7 +30,13 @@ urlpatterns = [
     path('', index_page),
     path('words_page/', words_app),
     url('', include('social_django.urls', namespace='social')),
-    path('auth/', auth)
-]
+    path('auth/', auth),
+    ]
 
 urlpatterns += router.urls
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] + urlpatterns
