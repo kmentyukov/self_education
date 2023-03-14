7f67479efbb9a25bb17bf9e2764e35d4bc3aba39
Kirill Mentyukov <rogue5@mail.ru>
2023-03-14 22:30:52 +0600

    edit m2m field in word-model through UserWord-model, add new attributes to model

--- self_edu/english/admin.py
+++ self_edu/english/admin.py
@@ -6,7 +6,7 @@ from english.models import Word
 
 @admin.register(Word)
 class WordAdmin(ModelAdmin):
-    list_display = ('id', 'en_word', 'ru_word', 'ru_word_optional', 'show_num', 'already_learn')
+    list_display = ('id', 'en_word', 'ru_word', 'ru_word_optional')
     list_display_links = ('id', 'en_word', 'ru_word', 'ru_word_optional')
     search_fields = ('en_word', 'ru_word', 'ru_word_optional')
-    list_filter = ('already_learn',)
+
--- self_edu/english/models.py
+++ self_edu/english/models.py
@@ -7,8 +7,7 @@ class Word(models.Model):
     en_word = models.CharField(max_length=50, unique=True, db_index=True, verbose_name='English word')
     ru_word = models.CharField(max_length=50, verbose_name='Russian translation', blank=True)
     ru_word_optional = models.CharField(max_length=50, verbose_name='Additional russian translation', blank=True)
-    show_num = models.IntegerField(verbose_name='Number of shows', default=0)
-    users = models.ManyToManyField(User, through='UserWord')
+    users = models.ManyToManyField(User, through='UserWord', through_fields=('word', 'user'))
 
     def __str__(self):
         return f'ID {self.id}: {self.en_word}'
@@ -25,4 +24,13 @@ class Word(models.Model):
 class UserWord(models.Model):
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     word = models.ForeignKey(Word, null=False, blank=False, on_delete=models.CASCADE)
+    show_num = models.IntegerField(verbose_name='Number of shows', default=0)
     already_learn = models.BooleanField(verbose_name='Learned', default=False)
+
+    class Meta:
+        verbose_name = 'Английское слово'
+        verbose_name_plural = 'Английские слова'
+
+    def __str__(self):
+        return self.word
+
