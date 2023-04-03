8d5506e341a5073a59743a63b567ec7305f46166
Kirill Mentyukov <rogue5@mail.ru>
2023-04-03 23:11:00 +0300

    edit main model

--- self_edu/english/models.py
+++ self_edu/english/models.py
@@ -7,7 +7,9 @@ class Word(models.Model):
     en_word = models.CharField(max_length=50, unique=True, db_index=True, verbose_name='English word')
     ru_word = models.CharField(max_length=50, verbose_name='Russian translation', blank=True)
     ru_word_optional = models.CharField(max_length=50, verbose_name='Additional russian translation', blank=True)
-    users = models.ManyToManyField(User, through='UserWord', through_fields=('word', 'user'))
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    show_num = models.IntegerField(verbose_name='Number of shows', default=0)
+    already_learn = models.BooleanField(verbose_name='Learned', default=False)
 
     def __str__(self):
         return f'ID {self.id}: {self.en_word}'
